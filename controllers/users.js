62fbdc896778142ce47aea9e200099e66f5d9f4a
Screench <120321464+Screench@users.noreply.github.com>
2023-07-26 17:22:16 +0300

    fix after test

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,4 @@
-//Контроллер пользователя
 const User = require('../models/user');
-
 const { INCORRECT_DATA_ERROR, DOCUMENT_NOT_FOUND_ERROR, UNKNOWN_ERROR } = require('../errors/errors');
 
 const getUsers = (req, res) => {
@@ -16,10 +14,10 @@ const getUsers = (req, res) => {
 
 const getUserById = (req, res) => {
   User.findById(req.params.userId)
-  .orFail(() => new Error("Not Found"))
+    .orFail(() => new Error("Not Found"))
     .then((user) => res.send(user))
     .catch((err) => {
-      if (err.name === "Data Error") {
+      if (err.name === "DataError") {
         res.status(INCORRECT_DATA_ERROR).send({
           message: 'Переданы некорректные данные'
         });
@@ -42,7 +40,7 @@ const createUser = (req, res) => {
       res.send(user)
     })
     .catch(err => {
-      if (!name || !avatar || err) {
+      if (!name || !avatar || err.message) {
         res.status(INCORRECT_DATA_ERROR).send({ message: 'Переданы некорректные данные' });
         return;
       } else {
@@ -51,17 +49,21 @@ const createUser = (req, res) => {
     });
 };
 
+
+
 const updateProfile = (req, res) => {
   const { name, about } = req.body;
   const { _id } = req.user;
   User.findByIdAndUpdate(_id, { name, about }, { new: true, runValidators: true })
     .then((user) => res.send(user))
     .catch(err => {
-      if (!name || !about || err) {
+      if (!name || !about || err.message) {
         res.status(INCORRECT_DATA_ERROR).send({ message: 'Переданы некорректные данные' });
         return;
-      }
-      else {
+      } else if (!User[_id]) {
+        res.status(DOCUMENT_NOT_FOUND_ERROR).send({ message: 'Такого пользователя нет' });
+        return;
+      } else {
         res.status(UNKNOWN_ERROR).send({ message: 'Неизвестная ошибка', err: err.message })
       }
     })
