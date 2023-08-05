324b8352826cd5fa621f02fb54a89544b3e05a40
Screench <120321464+Screench@users.noreply.github.com>
2023-08-06 01:15:31 +0300

    added limiter

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
 const helmet = require('helmet');
+const limiter = require('./middleware/rateLimit');
 const router = require('./routes');
 const errorHandler = require('./middleware/errorHandler');
 
@@ -11,6 +12,7 @@ const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.en
 
 const app = express();
 app.use(helmet());
+app.use(limiter);
 app.use(express.json());
 app.use(cookieParser());
 
